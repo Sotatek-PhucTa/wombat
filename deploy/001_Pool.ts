aa50b0dba8ebd986fd4f79d4d9aa2ee52584be7f
jack <97011001+jack-wombat@users.noreply.github.com>
2022-02-13 20:24:41 -0800

    Fix lint

--- deploy/001_Pool.ts
+++ deploy/001_Pool.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'hardhat'
-import { parseEther, parseUnits } from '@ethersproject/units'
+import { parseEther } from '@ethersproject/units'
 
 const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
