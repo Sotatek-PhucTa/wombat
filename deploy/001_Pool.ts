10d696e4dfcc7444a5fd9a2a815dc216b4513d6e
drop19 <drop19@example.com>
2022-02-28 21:26:28 +0800

    Revert "update pool script to use proxy"
    This reverts commit 5f77a7100e8064f2ade1f841016a15cdc660e7a4.

--- deploy/001_Pool.ts
+++ deploy/001_Pool.ts
@@ -1,41 +1,56 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import { ethers, upgrades } from 'hardhat'
+import { ethers } from 'hardhat'
 import { parseEther } from '@ethersproject/units'
 
 const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { getNamedAccounts } = hre
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
+
+  // Get Signers
+  const [owner] = await ethers.getSigners()
 
   console.log(`Step 001. Deploying on : ${hre.network.name} with account : ${deployer}`)
 
-  const PoolFactory = await ethers.getContractFactory('Pool', deployer)
-
-  // Deploy and initialize pool contract
-  const poolDeployResult = await upgrades.deployProxy(PoolFactory, [parseEther('0.001'), parseEther('0.0001')], {
-    unsafeAllow: ['delegatecall'], // allow unsafe delegate call as SafeERC20 is no upgradable
-    kind: 'uups',
+  /// Deploy pool
+  const poolDeployResult = await deploy('Pool', {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
   })
 
-  if (
-    hre.network.name == 'localhost' ||
-    hre.network.name == 'hardhat' ||
-    hre.network.name == 'bsc_testnet' ||
-    hre.network.name == 'bsc_mainnet'
-  ) {
-    // Get freshly deployed Pool contract
-    const pool = await ethers.getContractAt('Pool', poolDeployResult.address)
+  if (poolDeployResult.newlyDeployed) {
+    if (
+      hre.network.name == 'localhost' ||
+      hre.network.name == 'hardhat' ||
+      hre.network.name == 'bsc_testnet' ||
+      hre.network.name == 'bsc_mainnet'
+    ) {
+      // Get freshly deployed Pool contract
+      const pool = await ethers.getContractAt('Pool', poolDeployResult.address)
 
-    // Check dev account
-    const dev = await pool.dev()
-    console.log(`Dev account is : ${dev}`)
-    console.log(`Deployer account is: ${deployer}`)
+      // Check dev account
+      const dev = await pool.dev()
+      console.log(`Dev account is : ${dev}`)
+      console.log(`Deployer account is: ${deployer}`)
 
-    // Check setup config values
-    const ampFactor = await pool.getAmpFactor()
-    const hairCutRate = await pool.getHaircutRate()
-    console.log(`Amplification factor is : ${ampFactor}`)
-    console.log(`Haircut rate is : ${hairCutRate}`)
+      // call initialize
+      if (ethers.utils.getAddress(dev) != ethers.utils.getAddress(deployer)) {
+        await pool.connect(owner).initialize(parseEther('0.001'), parseEther('0.0001'))
+      }
+
+      if (ethers.utils.getAddress(dev) == ethers.utils.getAddress(deployer)) {
+        await pool.connect(owner).setAmpFactor(parseEther('0.001'), { gasLimit: 300000 })
+        await pool.connect(owner).setHaircutRate(parseEther('0.0001'), { gasLimit: 300000 })
+
+        // Check setup config values
+        const ampFactor = await pool.getAmpFactor()
+        const hairCutRate = await pool.getHaircutRate()
+        console.log(`Amplification factor is : ${ampFactor}`)
+        console.log(`Haircut rate is : ${hairCutRate}`)
+      }
+    }
   }
 }
 
