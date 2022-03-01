16c3b6b0ea01837add119126a8f446eeaf12fd00
drop19 <drop19@example.com>
2022-03-01 22:37:52 +0800

    update deploy script for WOM, masterWombat, VeWOM

--- deploy/004_WOM.ts
+++ deploy/004_WOM.ts
@@ -1,30 +1,32 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { DeployFunction } from 'hardhat-deploy/types'
-import { parseUnits } from '@ethersproject/units'
+import { parseEther } from '@ethersproject/units'
 import { ethers } from 'hardhat'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
 interface IWombatToken {
   [token: string]: unknown[]
 }
 
 export const WOMBAT_TOKENS_ARGS: IWombatToken = {
-  WOM: ['Wombat Token', 'WOM', '18', 1000000], // 1,000,000 tokens minted to msg.sender initially
+  WOM: ['Wombat Token', 'WOM', '18', 1e9], // 1b tokens minted to msg.sender initially
 }
 
-const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const contractName = 'WombatToken'
+
+const deployFunc = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
-  console.log(`Step 004. Deploying on : ${hre.network.name} with account : ${deployer}`)
+  console.log(`Step 101. Deploying on : ${hre.network.name} with account : ${deployer}`)
 
   /// Deploy pool
-  const womDeployResult = await deploy('WOM', {
+  const womDeployResult = await deploy(contractName, {
     from: deployer,
     contract: 'WombatERC20',
     log: true,
-    args: [parseUnits('1000000', 18)], // 1,000,000 tokens minted to msg.sender initially
+    args: [parseEther('1000000000')], // 1b tokens minted to msg.sender initially
     skipIfAlreadyDeployed: true,
+    deterministicDeployment: true, // use CREATE2 for deterministic address
   })
 
   // Mock WOM token only on localhost and bsc testnet
@@ -43,6 +45,8 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
       console.log(`Deployer account is: ${deployer}`)
     }
   }
+
+  return womDeployResult
 }
 export default deployFunc
-deployFunc.tags = ['MockWombatToken']
+deployFunc.tags = [contractName]
--- deploy/102_VeWom.ts
+++ deploy/102_VeWom.ts
@@ -1,18 +1,19 @@
-import { parseEther } from '@ethersproject/units'
 import { ethers } from 'hardhat'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
 const proxyImplAddr = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc' // EIP1967
-const contractName = 'Pool'
+const contractName = 'VeWom'
 
 const deployFunc = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
-  console.log(`Step 001. Deploying on : ${hre.network.name} with account : ${deployer}`)
+  console.log(`Step 102. Deploying on : ${hre.network.name} with account : ${deployer}`)
 
-  /// Deploy pool
+  const wombatToken = await deployments.get('WombatToken')
+  const masterWombat = await deployments.get('MasterWombat')
+
   const deployResult = await deploy(contractName, {
     from: deployer,
     log: true,
@@ -24,12 +25,12 @@ const deployFunc = async function (hre: HardhatRuntimeEnvironment) {
       execute: {
         init: {
           methodName: 'initialize',
-          args: [parseEther('0.05'), parseEther('0.0001')],
+          args: [wombatToken.address, masterWombat.address],
         },
       },
     },
+    deterministicDeployment: true, // use CREATE2 for deterministic address
   })
-
   // Get freshly deployed Pool contract
   const contract = await ethers.getContractAt(contractName, deployResult.address)
   const implAddr = await contract.provider.getStorageAt(deployResult.address, proxyImplAddr)
@@ -37,14 +38,17 @@ const deployFunc = async function (hre: HardhatRuntimeEnvironment) {
   console.log('Implementaion address:', implAddr)
 
   if (deployResult.newlyDeployed) {
+    const masterWombatContract = await ethers.getContractAt('MasterWombat', masterWombat.address)
+    await masterWombatContract.setVeWom(deployResult.address)
+
     // Check setup config values
-    const ampFactor = await contract.ampFactor()
-    const hairCutRate = await contract.haircutRate()
-    console.log(`Amplification factor is : ${ampFactor}`)
-    console.log(`Haircut rate is : ${hairCutRate}`)
+    const womTokenAddress = await contract.wom()
+    const masterWombatAddress = await contract.masterWombat()
+    console.log(`WomTokenAddress is : ${womTokenAddress}`)
+    console.log(`MasterWombatAddress is : ${masterWombatAddress}`)
     return deployResult
   } else {
-    throw 'Error : Pool bytecode is unchanged. Please choose the correct NEW_POOL_CONTRACT_NAME'
+    throw 'Error : Bytecode is unchanged. Please choose the correct NEW_POOL_CONTRACT_NAME'
   }
 }
 
