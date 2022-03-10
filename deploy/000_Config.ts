29595257e3afbb27bdc07d6f0018ddd87716b694
Daniel Chan <danielchan.yes@gmail.com>
2022-03-10 15:28:27 +0800

    add config & improve 003_assets

---
+++ deploy/000_Config.ts
@@ -0,0 +1,40 @@
+// starting 4 stables, all 18 decimals
+interface IUSDTokens {
+  [network: string]: IUSDTTokensInfo
+}
+interface IUSDTTokensInfo {
+  [token: string]: unknown[]
+}
+
+export const USD_TOKENS_MAP: IUSDTokens = {
+  bsc_mainnet: {
+    BUSD: ['Binance USD', 'BUSD', '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'],
+    USDC: ['USD Coin', 'USDC', '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'],
+    USDT: ['Tether USD', 'USDT', '0x55d398326f99059ff775485246999027b3197955'],
+    DAI: ['Dai Stablecoin', 'DAI', '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'],
+  },
+  bsc_testnet: {
+    BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
+    USDC: ['USD Coin', 'USDC', '18', 0],
+    USDT: ['Tether USD', 'USDT', '18', 0],
+    TUSD: ['TrueUSD', 'TUSD', '18', 0],
+    DAI: ['Dai Stablecoin', 'DAI', '18', 0],
+    vUSDC: ['Venus USDC', 'vUSDC', '8', 0],
+  },
+  localhost: {
+    BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
+    USDC: ['USD Coin', 'USDC', '18', 0],
+    USDT: ['Tether USD', 'USDT', '18', 0],
+    TUSD: ['TrueUSD', 'TUSD', '18', 0],
+    DAI: ['Dai Stablecoin', 'DAI', '18', 0],
+    vUSDC: ['Venus USDC', 'vUSDC', '8', 0],
+  },
+  hardhat: {
+    BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
+    USDC: ['USD Coin', 'USDC', '18', 0],
+    USDT: ['Tether USD', 'USDT', '18', 0],
+    TUSD: ['TrueUSD', 'TUSD', '18', 0],
+    DAI: ['Dai Stablecoin', 'DAI', '18', 0],
+    vUSDC: ['Venus USDC', 'vUSDC', '8', 0],
+  },
+}
--- deploy/003_Assets.ts
+++ deploy/003_Assets.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'hardhat'
-import { USD_TOKENS_MAP } from './002_Mock_Tokens'
+import { USD_TOKENS_MAP } from './000_Config'
 
 const contractName = 'Asset'
 
@@ -17,6 +17,11 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
 
   // create asset contracts, e.g. LP-USDC, LP-BUSD, etc. for the ERC20 stablecoins list
   const USD_TOKENS = USD_TOKENS_MAP[hre.network.name]
+
+  // Get Pool Instance
+  const poolDeployment = await deployments.get('Pool')
+  const pool = await ethers.getContractAt('Pool', poolDeployment.address)
+
   for (const index in USD_TOKENS) {
     console.log('Attemping to deploy Asset contract : ' + USD_TOKENS[index][0])
     const tokenSymbol = USD_TOKENS[index][1] as string
@@ -35,29 +40,49 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
       args: [tokenAddress, name, symbol],
       skipIfAlreadyDeployed: true,
     })
+    const address = usdAssetDeployResult.address
 
-    // Add asset addresses & token reference to pool
+    // newly-deployed Asset
     if (usdAssetDeployResult.newlyDeployed) {
-      // Get Pool Instance
-      const poolDeployment = await deployments.get('Pool')
-      const pool = await ethers.getContractAt('Pool', poolDeployment.address)
-
-      // Add Asset to Pool
-      const addAssetTxn = await pool.connect(owner).addAsset(tokenAddress, usdAssetDeployResult.address)
-      // wait until the transaction is mined
-      await addAssetTxn.wait()
+      // Add new Asset to existing or newly-deployed Pool
+      await addAsset(pool, owner, tokenAddress, address)
 
       // Add pool reference to Asset
-      const asset = await ethers.getContractAt('Asset', usdAssetDeployResult.address)
-      const setPoolTxn = await asset.connect(owner).setPool(poolDeployment.address)
-      await setPoolTxn.wait()
+      const asset = await ethers.getContractAt('Asset', address)
+      await addPool(asset, owner, poolDeployment)
 
-      const address = usdAssetDeployResult.address
       console.log(`Added ${tokenSymbol} Asset at ${address} to Pool located ${poolDeployment.address}`)
       console.log(
         `To verify, run: hh verify --network ${hre.network.name} ${address} ${tokenAddress} '${name}' '${symbol}'`
       )
+    } else {
+      // Add existing asset to existing or newly-deployed Pool
+      const assetDeployment = await deployments.get(`Asset_${tokenSymbol}`)
+      const existingAsset = await ethers.getContractAt('Asset', assetDeployment.address)
+      await addPool(existingAsset, owner, poolDeployment)
+
+      // Add Asset to existing or newly-deployed Pool
+      await addAsset(pool, owner, tokenAddress, address)
     }
+  }
+}
+
+async function addAsset(pool: any, owner: any, tokenAddress: string, assetAddress: string) {
+  try {
+    const addAssetTxn = await pool.connect(owner).addAsset(tokenAddress, assetAddress)
+    // wait until the transaction is mined
+    await addAssetTxn.wait()
+  } catch (err) {
+    // do nothing as asset already exists in pool
+  }
+}
+
+async function addPool(asset: any, owner: any, poolAddress: string) {
+  try {
+    const setPoolTxn = await asset.connect(owner).setPool(poolAddress)
+    await setPoolTxn.wait()
+  } catch (err) {
+    // this should not happen
   }
 }
 
