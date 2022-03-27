57566421f009b194a76c306757f6bbaa417f306b
drop19 <oscar@wombat.exchange>
2022-03-27 23:29:09 +0800

    temp

--- deploy/101_MasterWombat.ts
+++ deploy/101_MasterWombat.ts
@@ -1,6 +1,7 @@
 import { BigNumber } from 'ethers'
 import { ethers } from 'hardhat'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { USD_TOKENS_MAP } from '../tokens.config'
 
 const contractName = 'MasterWombat'
 
@@ -39,6 +40,16 @@ const deployFunc = async function (hre: HardhatRuntimeEnvironment) {
   const implAddr = await upgrades.erc1967.getImplementationAddress(deployResult.address)
   console.log('Contract address:', deployResult.address)
   console.log('Implementation address:', implAddr)
+
+  const USD_TOKENS = USD_TOKENS_MAP[hre.network.name]
+  for (const index in USD_TOKENS) {
+    const tokenSymbol = USD_TOKENS[index][1] as string
+    const assetContractName = `Asset_${tokenSymbol}_V2`
+    const assetContractAddress = (await deployments.get(assetContractName)).address as string
+
+    console.log('Adding asset', assetContractAddress)
+    await contract.connect(deployer).add(10, assetContractAddress, ethers.constants.AddressZero)
+  }
 
   if (deployResult.newlyDeployed) {
     const poolContract = await ethers.getContractAt('Pool', pool.address)
