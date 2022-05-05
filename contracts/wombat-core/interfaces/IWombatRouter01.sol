475b9600e7e2454ca37eb2445a042d90714c2e5b
corey-wombat <corey@wombat.exchange>
2022-05-05 12:50:45 +0800

    Add router

---
+++ contracts/wombat-core/interfaces/IWombatRouter01.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.5;
+
+interface IWombatRouter01 {
+    function swapTokensForTokens(
+        address[] calldata tokenPath,
+        address[] calldata poolPath,
+        uint256 fromAmount,
+        uint256 minimumToAmount,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountOut, uint256 haircut);
+}
---
+++ test/helpers/helper.ts
@@ -0,0 +1,23 @@
+import { ethers } from 'hardhat'
+import chai from 'chai'
+import { BigNumber, BigNumberish, Contract, ContractFactory, Signer } from 'ethers'
+import { solidity } from 'ethereum-waffle'
+
+const { expect } = chai
+chai.use(solidity)
+
+export function toWei(amount: BigNumberish, tokenDecimals: BigNumberish): BigNumber {
+  return ethers.utils.parseUnits(amount.toString(), tokenDecimals)
+}
+
+export const expectAssetValues = async (
+  asset: Contract,
+  decimals: number,
+  { cash, liability, tokenBN }: { cash: string; liability: string; tokenBN: string }
+): Promise<void> => {
+  const expectedCashBN = toWei(cash, 18)
+
+  expect(await asset.cash()).to.be.equal(expectedCashBN)
+  expect(await asset.liability()).to.be.equal(toWei(liability, 18))
+  expect(await asset.underlyingTokenBalance()).to.be.equal(toWei(tokenBN, decimals))
+}
