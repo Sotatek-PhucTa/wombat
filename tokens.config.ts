eb09fcd5971ab54919a42585e9aa77d9b6f0f44c
daniel-wombat <daniel@wombat.exchange>
2022-07-23 14:21:56 +0800

    update configs

--- tokens.config.ts
+++ tokens.config.ts
@@ -38,3 +38,34 @@ export const USD_TOKENS_MAP: IUSDTokens = {
     vUSDC: ['Venus USDC', 'vUSDC', '8', 0],
   },
 }
+
+export const USD_SIDEPOOL_TOKENS_MAP: IUSDTokens = {
+  bsc_mainnet: {
+    BUSD: ['Binance USD', 'BUSD', '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'],
+    TUSD: ['TrueUSD', 'TUSD', '0x14016E85a25aeb13065688cAFB43044C2ef86784'],
+    FRAX: ['Frax', 'FRAX', '0x90C97F71E18723b0Cf0dfa30ee176Ab653E89F40'],
+    MIM: ['Magic Internet Money', 'MIM', '0xfE19F0B51438fd612f6FD59C1dbB3eA319f433Ba'],
+    HAY: ['Hay Stablecoin', 'HAY', ''], // pend to launch on mainnet
+  },
+  bsc_testnet: {
+    BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
+    TUSD: ['TrueUSD', 'TUSD', '18', 0],
+    FRAX: ['Frax', 'FRAX', '18', 0],
+    MIM: ['Magic Internet Money', 'MIM', '18', 0],
+    HAY: ['Hay Stablecoin', 'HAY', '18', 0],
+  },
+  localhost: {
+    BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
+    TUSD: ['TrueUSD', 'TUSD', '18', 0],
+    FRAX: ['Frax', 'FRAX', '18', 0],
+    MIM: ['Magic Internet Money', 'MIM', '18', 0],
+    HAY: ['Hay Stablecoin', 'HAY', '18', 0],
+  },
+  hardhat: {
+    BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
+    TUSD: ['TrueUSD', 'TUSD', '18', 0],
+    FRAX: ['Frax', 'FRAX', '18', 0],
+    MIM: ['Magic Internet Money', 'MIM', '18', 0],
+    HAY: ['Hay Stablecoin', 'HAY', '18', 0],
+  },
+}
