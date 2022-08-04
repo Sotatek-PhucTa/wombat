3ea97fbe49fbfaf7322c67a555e6d46b5c96ca0c
daniel-wombat <daniel@wombat.exchange>
2022-08-04 12:14:13 +0800

    update deploy scripts

---
+++ deploy/003_DynamicPool.ts
@@ -0,0 +1,76 @@
+import { parseEther } from '@ethersproject/units'
+import { ethers } from 'hardhat'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+const contractName = 'DynamicPool_01'
+
+const deployFunc = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, upgrades } = hre
+  const { deploy } = deployments
+  const { deployer, multisig } = await getNamedAccounts()
+  const [owner] = await ethers.getSigners() // first account used for testnet and mainnet
+
+  console.log(`Step 003. Deploying on : ${hre.network.name}...`)
+
+  /// Deploy sidepool
+  const deployResult = await deploy(contractName, {
+    from: deployer,
+    log: true,
+    contract: 'DynamicPool', // Use latest (if any) with 'DynamicPool' after audit
+    skipIfAlreadyDeployed: true,
+    proxy: {
+      owner: multisig, // change to Gnosis Safe after all admin scripts are done
+      proxyContract: 'OptimizedTransparentProxy',
+      viaAdminContract: 'DefaultProxyAdmin',
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [parseEther('0.02'), parseEther('0.001')], // [A, haircut] are 10x of main pool
+        },
+      },
+    },
+  })
+
+  // Get freshly deployed DynamicPool contract
+  const contract = await ethers.getContractAt('DynamicPool', deployResult.address)
+  const implAddr = await upgrades.erc1967.getImplementationAddress(deployResult.address)
+  console.log('Contract address:', deployResult.address)
+  console.log('Implementation address:', implAddr)
+
+  if (deployResult.newlyDeployed) {
+    // Check setup config values
+    const ampFactor = await contract.ampFactor()
+    const hairCutRate = await contract.haircutRate()
+    console.log(`Amplification factor is : ${ampFactor}`)
+    console.log(`Haircut rate is : ${hairCutRate}`)
+
+    if (hre.network.name == 'bsc_mainnet') {
+      // manually transfer proxyAdmin to multi-sig, do it once and all proxy contracts will follow suit
+      // The owner of the ProxyAdmin can upgrade our contracts
+      // The owner of the pool contract is very powerful!
+
+      // transfer pool contract dev to Gnosis Safe
+      console.log(`Transferring dev of ${deployResult.address} to ${multisig}...`)
+      // The dev of the pool contract can pause and unpause pools & assets!
+      const setDevTxn = await contract.connect(owner).setDev(multisig)
+      await setDevTxn.wait()
+      console.log(`Transferred dev of ${deployResult.address} to:`, multisig)
+
+      /// Admin scripts
+      console.log(`setFee to 0 for lpDividendRatio and ${10 ** 18} for retentionRatio...`)
+      const setFeeTxn = await contract.connect(owner).setFee(0, parseEther('1'))
+      await setFeeTxn.wait()
+
+      console.log(`setMintFeeThreshold to ${10000 ** 18}...`)
+      const setMintFeeThresholdTxn = await contract.connect(owner).setMintFeeThreshold(parseEther('1000'))
+      await setMintFeeThresholdTxn.wait()
+    }
+    return deployResult
+  } else {
+    console.log(`${contractName} Contract already deployed.`)
+    return deployResult
+  }
+}
+
+export default deployFunc
+deployFunc.tags = [contractName]
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,12 +1,12 @@
 // starting 4 stables, all 18 decimals
-interface IUSDTokens {
-  [network: string]: IUSDTTokensInfo
+interface ITokens {
+  [network: string]: ITokensInfo
 }
-interface IUSDTTokensInfo {
+interface ITokensInfo {
   [token: string]: unknown[]
 }
 
-export const USD_TOKENS_MAP: IUSDTokens = {
+export const USD_TOKENS_MAP: ITokens = {
   bsc_mainnet: {
     BUSD: ['Binance USD', 'BUSD', '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'],
     USDC: ['USD Coin', 'USDC', '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'],
@@ -39,13 +39,13 @@ export const USD_TOKENS_MAP: IUSDTokens = {
   },
 }
 
-export const USD_SIDEPOOL_TOKENS_MAP: IUSDTokens = {
+export const USD_SIDEPOOL_TOKENS_MAP: ITokens = {
   bsc_mainnet: {
     BUSD: ['Binance USD', 'BUSD', '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'],
     TUSD: ['TrueUSD', 'TUSD', '0x14016E85a25aeb13065688cAFB43044C2ef86784'],
     FRAX: ['Frax', 'FRAX', '0x90C97F71E18723b0Cf0dfa30ee176Ab653E89F40'],
     MIM: ['Magic Internet Money', 'MIM', '0xfE19F0B51438fd612f6FD59C1dbB3eA319f433Ba'],
-    HAY: ['Hay Stablecoin', 'HAY', ''], // pend to launch on mainnet
+    HAY: ['Hay Stablecoin', 'HAY', ''], // TBC on mainnet
   },
   bsc_testnet: {
     BUSD: ['Binance USD', 'BUSD', '18', 0], // 0 tokens minted to msg.sender initially
@@ -67,5 +67,70 @@ export const USD_SIDEPOOL_TOKENS_MAP: IUSDTokens = {
     FRAX: ['Frax', 'FRAX', '18', 0],
     MIM: ['Magic Internet Money', 'MIM', '18', 0],
     HAY: ['Hay Stablecoin', 'HAY', '18', 0],
+  },
+}
+
+export const BNB_DYNAMICPOOL_TOKENS_MAP: ITokens = {
+  bsc_mainnet: {
+    WBNB: ['Wrapped BNB', 'WBNB', '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', '', 'Dynamic'], // last 2 items are exchange rate oracle and asset type
+    STKBNB: ['Staked BNB', 'stkBNB', '', '', 'Stkbnb'], // TBC on mainnet
+    BNBX: [
+      'Liquid Staking BNB',
+      'BNBx',
+      '0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275',
+      '0x7276241a669489E4BBB76f63d2A43Bfe63080F2F',
+      'Bnbx'
+    ],
+  },
+  bsc_testnet: {
+    WBNB: ['Wrapped BNB', 'WBNB', '18', 0, 'Dynamic'],
+    STKBNB: [
+      'Staked BNB',
+      'stkBNB',
+      '0xF7CE8444b3b1c62e785a25343a8B4764198A81B8',
+      '0x7CdFba1Ee6A8D1e688B4B34A56b62287ce400802',
+      'Stkbnb'
+    ],
+    BNBX: [
+      'Liquid Staking BNB',
+      'BNBx',
+      '0x3ECB02c703C815e9cFFd8d9437B7A2F93638d7Cb',
+      '0xDAdcae6bF110c0e70E5624bCdcCBe206f92A2Df9',
+      'Bnbx'
+    ],
+  },
+  localhost: {
+    WBNB: ['Wrapped BNB', 'WBNB', '18', 0, 'Dynamic'],
+    STKBNB: [
+      'Staked BNB',
+      'stkBNB',
+      '0xF7CE8444b3b1c62e785a25343a8B4764198A81B8',
+      '0x7CdFba1Ee6A8D1e688B4B34A56b62287ce400802',
+      'Stkbnb',
+    ],
+    BNBX: [
+      'Liquid Staking BNB',
+      'BNBx',
+      '0x3ECB02c703C815e9cFFd8d9437B7A2F93638d7Cb',
+      '0xDAdcae6bF110c0e70E5624bCdcCBe206f92A2Df9',
+      'Bnbx'
+    ],
+  },
+  hardhat: {
+    WBNB: ['Wrapped BNB', 'WBNB', '18', 0, 'Dynamic'],
+    STKBNB: [
+      'Staked BNB',
+      'stkBNB',
+      '0xF7CE8444b3b1c62e785a25343a8B4764198A81B8',
+      '0x7CdFba1Ee6A8D1e688B4B34A56b62287ce400802',
+      'Stkbnb'
+    ],
+    BNBX: [
+      'Liquid Staking BNB',
+      'BNBx',
+      '0x3ECB02c703C815e9cFFd8d9437B7A2F93638d7Cb',
+      '0xDAdcae6bF110c0e70E5624bCdcCBe206f92A2Df9',
+      'Bnbx'
+    ],
   },
 }
