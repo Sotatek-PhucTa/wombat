e33facbdabec257d0d9eccfffc8244e758ffdc08
drop19 <93721169+drop19@users.noreply.github.com>
2022-09-03 14:15:11 +0800

    Remove unused contracts (#142)

--- contracts/wombat-governance/mocks/MockVeWom.sol
+++ contracts/wombat-governance/mocks/MockVeWom.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.5;
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '../VeWom.sol';
-import '../MasterWombat.sol';
 
 contract MockVeWom is Initializable, VeWom {
     function faucet(uint256 _amount) public {
