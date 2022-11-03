4662e753e47ece9403d2ec082160471144f17c23
jack <97011001+jack-wombat@users.noreply.github.com>
2022-11-03 11:42:40 +0800

    Voter deploy script to add LPs
    Before:
    Voter is missing LPs so migrate fails on MWv2
    
    After:
    Able to migrate BUSD from MWv2 to MWv3
    https://testnet.bscscan.com/tx/0x062e61c458114fd6a20b6c885a6bee4db8d8690df0fdbfb38aaed6febf373f2a
    
    Summary:
    Add support for side and wom pools
    
    Fix a bug where alloc point becomes undefined because of pop. Use slice
    to create a new copy instead of mutating the shared config.

--- deploy/001_Mock_Tokens.ts
+++ deploy/001_Mock_Tokens.ts
@@ -61,8 +61,8 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
     const WOM_SIDEPOOL_TOKENS = WOM_DYNAMICPOOL_TOKENS_MAP[hre.network.name]
     for (const tokens of Object.values(WOM_SIDEPOOL_TOKENS)) {
       for (const deployArgs of Object.values(tokens)) {
-        deployArgs.pop()
-        const tokenSymbol = deployArgs[1] as string
+        const args = deployArgs.slice(0, -1)
+        const tokenSymbol = args[1] as string
         await deploy(tokenSymbol, {
           from: deployer,
           log: true,
--- deploy/132_VoterSetup.ts
+++ deploy/132_VoterSetup.ts
@@ -1,17 +1,24 @@
 import { ethers } from 'hardhat'
-import { BigNumber } from 'ethers'
+import { Contract } from 'ethers'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { BNB_DYNAMICPOOL_TOKENS_MAP, USD_TOKENS_MAP } from '../tokens.config'
+import {
+  WOM_DYNAMICPOOL_TOKENS_MAP,
+  BNB_DYNAMICPOOL_TOKENS_MAP,
+  USD_SIDEPOOL_TOKENS_MAP,
+  USD_TOKENS_MAP,
+} from '../tokens.config'
 import { getDeployedContract, confirmTxn } from '../utils'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
 const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, upgrades } = hre
+  const { deployments } = hre
   const [owner] = await ethers.getSigners() // first account used for testnet and mainnet
 
   console.log(`Step 132. Deploying on: ${hre.network.name}...`)
 
   const voter = await getDeployedContract('Voter')
+  const masterWombat = await getDeployedContract('MasterWombatV3')
 
   console.log('Setting up main pool')
   const USD_TOKENS = USD_TOKENS_MAP[hre.network.name]
@@ -20,6 +27,18 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
     const tokenAllocPoint = USD_TOKENS[index][3] as number
     const assetContractName = `Asset_P01_${tokenSymbol}`
     const assetContractAddress = (await deployments.get(assetContractName)).address as string
+    await addAsset(voter, owner, masterWombat.address, assetContractAddress)
+    await setAllocPoint(voter, owner, assetContractAddress, tokenAllocPoint)
+  }
+
+  console.log('Setting up side pool')
+  const USD_SIDEPOOL_TOKENS = USD_SIDEPOOL_TOKENS_MAP[hre.network.name]
+  for (const index in USD_SIDEPOOL_TOKENS) {
+    const tokenSymbol = USD_SIDEPOOL_TOKENS[index][1] as string
+    const tokenAllocPoint = USD_SIDEPOOL_TOKENS[index][3] as number
+    const assetContractName = `Asset_SP01_${tokenSymbol}`
+    const assetContractAddress = (await deployments.get(assetContractName)).address as string
+    await addAsset(voter, owner, masterWombat.address, assetContractAddress)
     await setAllocPoint(voter, owner, assetContractAddress, tokenAllocPoint)
   }
 
@@ -30,11 +49,31 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
     const tokenAllocPoint = BNB_DYNAMICPOOL_TOKENS[index][5] as number
     const assetContractName = `Asset_DP01_${tokenSymbol}`
     const assetContractAddress = (await deployments.get(assetContractName)).address as string
+    await addAsset(voter, owner, masterWombat.address, assetContractAddress)
     await setAllocPoint(voter, owner, assetContractAddress, tokenAllocPoint)
+  }
+
+  console.log('Setting up wom pool')
+  const WOM_DYNAMICPOOL_TOKENS = WOM_DYNAMICPOOL_TOKENS_MAP[hre.network.name]
+  for (const pool in WOM_DYNAMICPOOL_TOKENS) {
+    const WOM_POOL_TOKENS = WOM_DYNAMICPOOL_TOKENS[pool]
+    for (const index in WOM_POOL_TOKENS) {
+      const tokenSymbol = WOM_POOL_TOKENS[index][1] as string
+      const tokenAllocPoint = WOM_POOL_TOKENS[index][3] as number
+      const assetContractName = `Asset_${pool}_${tokenSymbol}`
+      const assetContractAddress = (await deployments.get(assetContractName)).address as string
+      await addAsset(voter, owner, masterWombat.address, assetContractAddress)
+      await setAllocPoint(voter, owner, assetContractAddress, tokenAllocPoint)
+    }
   }
 }
 
-async function setAllocPoint(voter: any, owner: any, assetAddress: string, tokenAllocPoint: number) {
+async function addAsset(voter: Contract, owner: SignerWithAddress, masterWombat: string, assetAddress: string) {
+  console.log('addAsset', assetAddress)
+  await confirmTxn(voter.connect(owner).add(masterWombat, assetAddress, ethers.constants.AddressZero))
+}
+
+async function setAllocPoint(voter: Contract, owner: SignerWithAddress, assetAddress: string, tokenAllocPoint: number) {
   console.log('setAllocPoint', assetAddress, tokenAllocPoint)
   try {
     await confirmTxn(voter.connect(owner).setAllocPoint(assetAddress, tokenAllocPoint), 2)
