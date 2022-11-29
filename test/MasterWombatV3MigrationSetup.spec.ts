ea8ec44da5dcd4663d80d4d5d62f22d614231f35
jack <97011001+jack-wombat@users.noreply.github.com>
2022-11-29 03:21:04 -0800

    Set up voter and mwv3

--- test/MasterWombatV3MigrationSetup.spec.ts
+++ test/MasterWombatV3MigrationSetup.spec.ts
@@ -3,14 +3,28 @@ import { deployments } from 'hardhat'
 import { getDeployedContract } from '../utils'
 
 describe('MasterWombatV3Migration', function () {
+  let masterWombatV3: Contract
+  let voter: Contract
   let pool: Contract
+  let busd: Contract
+  let busdAsset: Contract
 
   beforeEach(async function () {
     await deployments.fixture(['Asset', 'MockTokens', 'MasterWombatV3', 'Pool', 'Voter'])
-    ;[pool] = await Promise.all([getDeployedContract('Pool')])
+    ;[masterWombatV3, voter, pool, busd, busdAsset] = await Promise.all([
+      getDeployedContract('MasterWombatV3'),
+      getDeployedContract('Voter'),
+      getDeployedContract('Pool'),
+      getDeployedContract('TestERC20', 'BUSD'),
+      getDeployedContract('Asset', 'Asset_P01_BUSD'),
+    ])
   })
 
   it('works', async function () {
-    console.log('jack', pool.address)
+    console.log('mwv3', masterWombatV3.address)
+    console.log('voter', voter.address)
+    console.log('pool', pool.address)
+    console.log('busd', busd.address)
+    console.log('lp-busd', busdAsset.address)
   })
 })
