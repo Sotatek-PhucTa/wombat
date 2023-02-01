34edaa37856297055370715eecda5a5d76dc9fa5
jack <97011001+jack-wombat@users.noreply.github.com>
2023-01-31 21:49:12 -0800

    Add invariant test for FakeMegaPool

--- test/pool/invariant/FakeMegaPool.spec.ts
+++ test/pool/invariant/FakeMegaPool.spec.ts
@@ -3,8 +3,8 @@ import { expect } from 'chai'
 import { BigNumber, BigNumberish, Contract } from 'ethers'
 import { parseEther } from 'ethers/lib/utils'
 import { deployments, ethers } from 'hardhat'
-import { getTestERC20 } from '../../utils'
-import { deployTestAsset } from '../../utils/deploy'
+import { getTestERC20 } from '../../../utils'
+import { deployTestAsset } from '../../../utils/deploy'
 
 describe('FakeMegaPool', function () {
   let owner: SignerWithAddress
@@ -29,13 +29,16 @@ describe('FakeMegaPool', function () {
     await pool.addAsset(usdt.address, usdtAsset.address)
     await usdcAsset.setPool(pool.address)
     await usdtAsset.setPool(pool.address)
-    await usdc.faucet(parseEther('10000'))
-    await usdt.faucet(parseEther('10000'))
-    await deposit(owner, 10000, usdc)
-    await deposit(owner, 10000, usdt)
   })
 
   describe('swap', function () {
+    beforeEach(async function () {
+      await usdc.faucet(parseEther('10000'))
+      await usdt.faucet(parseEther('10000'))
+      await deposit(owner, 10000, usdc)
+      await deposit(owner, 10000, usdt)
+    })
+
     it('swap is the same as before', async function () {
       const [quoteAmount, quoteHaircut] = await quote(usdc, usdt, 777)
       const [actualToAmount, actualHaircut] = await swap(owner, usdc, usdt, 777)
@@ -46,7 +49,43 @@ describe('FakeMegaPool', function () {
       expect(quoteAmount).to.eq(777)
       expect(actualToAmount).to.eq(777 - 1)
     })
+
+    it('satisfies invariant', async function () {
+      await assertInvariant(owner, usdc, usdt, 777)
+    })
   })
+
+  describe('invariant', function () {
+    describe('balanced pool (5000 USDC, 5000 USDT)', function () {
+      beforeEach(async function () {
+        await usdc.faucet(parseEther('10000'))
+        await usdt.faucet(parseEther('10000'))
+        // leave 50% to swap
+        await deposit(owner, parseEther('5000'), usdc)
+        await deposit(owner, parseEther('5000'), usdt)
+      })
+
+      it.skip('satisfies invariant', async function () {
+        // Expected "99999920159742" to be equal 0
+        // toAmount: Expected "999899201677255258" to be greater than 999999201597410000
+        await assertInvariant(owner, usdc, usdt, parseEther('1'))
+      })
+    })
+  })
+
+  // assert that quote = swap (FakeMegaPool's token->credit->token).
+  async function assertInvariant(
+    user: SignerWithAddress,
+    fromToken: Contract,
+    toToken: Contract,
+    fromAmount: BigNumberish
+  ) {
+    const [quoteAmount, quoteHaircut] = await quote(fromToken, toToken, fromAmount)
+    const [actualToAmount, actualHaircut] = await swap(user, fromToken, toToken, fromAmount)
+    // allow for some precision error.
+    expect(quoteAmount, 'toAmount').to.gt(actualToAmount).lte(actualToAmount.add(1))
+    expect(quoteHaircut, 'haircut').to.eq(actualHaircut)
+  }
 
   async function deposit(user: SignerWithAddress, amount: BigNumberish, token: Contract) {
     await token.approve(pool.address, ethers.constants.MaxUint256)
