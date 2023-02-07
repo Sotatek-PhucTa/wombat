cf31da5c09980c84d3535a45ffd6b237b0a6a8d4
jack <97011001+jack-wombat@users.noreply.github.com>
2023-02-07 13:20:51 -0800

    Add test for balanced pool

--- test/pool/invariant/CoreV3.spec.ts
+++ test/pool/invariant/CoreV3.spec.ts
@@ -1,14 +1,54 @@
-import { Contract } from "ethers"
-import { deployments, ethers, getNamedAccounts } from "hardhat"
-import { deployContract } from "../../../utils/deploy"
+import { expect } from 'chai'
+import { BigNumber, BigNumberish, Contract } from 'ethers'
+import { parseEther } from 'ethers/lib/utils'
+import { ethers } from 'hardhat'
+import _ from 'lodash'
 
-describe('CoreV3', function() {
-    let core: Contract
-    beforeEach(async function () {
-        core = await ethers.deployContract('CoreV3')
-    })
+describe('CoreV3', function () {
+  const ampFactor = parseEther('0.04')
+  let core: Contract
 
-    it('runs', async function() {
-        // no op
+  // deploy once only since this is stateless
+  before(async function () {
+    core = await ethers.deployContract('CoreV3')
+  })
+
+  describe('balanced pool', function () {
+    ;[100, parseEther('5000')].map((tvl) => {
+      describe(`balanced pool (${tvl}) at equilibrium`, async function () {
+        _.range(0, 100).map((percent) => {
+          const swapAmount = BigNumber.from(tvl).mul(percent).div(100)
+          it(`ok to swap ${swapAmount}`, async function () {
+            await balancedPoolInvariant(tvl, swapAmount, ampFactor)
+          })
+        })
+      })
     })
+  })
+
+  // invariant for a balanced pool where cash and liability are all the same
+  async function balancedPoolInvariant(tvl: BigNumberish, swapAmount: BigNumberish, ampFactor: BigNumberish) {
+    return invariant(tvl, tvl, tvl, tvl, swapAmount, ampFactor)
+  }
+
+  // verify that quote = swapFromCredit(swapToCredit)
+  async function invariant(
+    fromAssetCash: BigNumberish,
+    fromAssetLiability: BigNumberish,
+    toAssetCash: BigNumberish,
+    toAssetLiability: BigNumberish,
+    swapAmount: BigNumberish,
+    ampFactor: BigNumberish
+  ) {
+    // compute quote and swapToCredit first since there are no data dependency
+    const [credit, quote] = await Promise.all([
+      core.swapToCreditQuote(fromAssetCash, fromAssetLiability, swapAmount, ampFactor),
+      core.swapQuoteFunc(fromAssetCash, toAssetCash, fromAssetLiability, toAssetLiability, swapAmount, ampFactor),
+    ])
+    const quoteFromCredit = await core.swapFromCreditQuote(toAssetCash, toAssetLiability, credit, ampFactor)
+
+    // expect absolute delta when quote is small; else relative.
+    const delta = quote.lt(parseEther('0.1')) ? 1 : quote.div(10000) // 1bps
+    expect(quoteFromCredit, 'quoteFromCredit').to.be.closeTo(quote, delta)
+  }
 })
