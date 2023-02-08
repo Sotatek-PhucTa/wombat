90210f11135ecc7162ba095467dc4397b884e040
jack <97011001+jack-wombat@users.noreply.github.com>
2023-02-07 22:39:31 -0800

    Add random testing for balanced pool

--- test/pool/invariant/CoreV3.spec.ts
+++ test/pool/invariant/CoreV3.spec.ts
@@ -2,15 +2,25 @@ import { expect } from 'chai'
 import { BigNumber, BigNumberish, Contract } from 'ethers'
 import { parseEther } from 'ethers/lib/utils'
 import { ethers } from 'hardhat'
+import { dynamicImport } from 'tsimportlib'
+import seedrandom from 'seedrandom'
 import _ from 'lodash'
 
 describe('CoreV3', function () {
   const ampFactor = parseEther('0.04')
   let core: Contract
+  let random: import('random').Random
 
   // deploy once only since this is stateless
   before(async function () {
     core = await ethers.deployContract('CoreV3')
+
+    const seed = process.env.RANDOM_SEED || +new Date()
+    // Workaround due to random being esm only
+    // solutions found in https://github.com/TypeStrong/ts-node/discussions/1290
+    const r = await dynamicImport('random', module)
+    random = new r.Random(new seedrandom(seed))
+    console.log(`initialized random using seed ${seed}`)
   })
 
   describe('balanced pool', function () {
@@ -19,7 +29,12 @@ describe('CoreV3', function () {
         it(`balanced pool (${tvl}) at equilibrium`, async function () {
           await Promise.all(
             _.range(0, 100).map((percent) => {
-              const swapAmount = BigNumber.from(tvl).mul(percent).div(100)
+              const bips = random.int(0, 99) // [0, 100)
+              const swapAmount = BigNumber.from(tvl)
+                .mul(percent)
+                .div(100)
+                .mul(10000 + bips)
+                .div(10000)
               return balancedPoolInvariant(tvl, swapAmount, ampFactor)
             })
           )
@@ -49,8 +64,17 @@ describe('CoreV3', function () {
     ])
     const quoteFromCredit = await core.swapFromCreditQuote(toAssetCash, toAssetLiability, credit, ampFactor)
 
+    // error message for reproducibility
+    const parameters = [
+      fromAssetCash,
+      fromAssetLiability,
+      fromAssetCash,
+      fromAssetLiability,
+      swapAmount,
+      ampFactor,
+    ].join(',')
     // expect absolute delta when quote is small; else relative.
     const delta = quote.lt(parseEther('0.1')) ? 1 : quote.div(10000) // 1bps
-    expect(quoteFromCredit, 'quoteFromCredit').to.be.closeTo(quote, delta)
+    expect(quoteFromCredit, `invariant failed for (${parameters}) quoteFromCredit`).to.be.closeTo(quote, delta)
   }
 })
