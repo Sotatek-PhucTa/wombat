fdafd3c990d474bee0a9fe24b0fdda4b36b6c5fc
corey-wombat <93235348+corey-wombat@users.noreply.github.com>
2023-03-04 09:30:35 +0800

    add random tests for CoreV3 (#305)
    * add random tests for CoreV3
    
    * minor fix
    
    * ditto
    
    * add data points

--- test/pool/invariant/CoreV3.spec.ts
+++ test/pool/invariant/CoreV3.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { BigNumber, BigNumberish, Contract } from 'ethers'
-import { parseEther } from 'ethers/lib/utils'
+import { formatEther, parseEther } from 'ethers/lib/utils'
 import { ethers } from 'hardhat'
 import { CreateRandom } from '../../../utils/random'
 import _ from 'lodash'
@@ -11,46 +11,177 @@ describe('CoreV3', function () {
   let core: Contract
   let random: Random
 
+  // @TODO: Add a function generate random TVL, cov ratio, ampFactor and swapAmount
+  // @TODO: Add randomness with pool ampFactor
+  const tvlList: BigNumber[] = [
+    parseEther('100000'), // 100k
+    parseEther('1000000'), // 1 mil
+    parseEther('5000000'), // 5 mil
+    parseEther('293000000'), // 293 mil
+    parseEther('467000000'), // 467 mil
+    parseEther('719000000'), // 719 mil
+    parseEther('1000000000'), // 1b
+  ]
+
   // deploy once only since this is stateless
   before(async function () {
     core = await ethers.deployContract('CoreV3')
-    const seed = process.env.RANDOM_SEED || new Date().toDateString()
+    const seed = process.env.RANDOM_SEED || new Date().toISOString()
     random = await CreateRandom(seed)
     // to reproduce, use `RANDOM_SEED=... hh test`
     console.log(`initialized random using seed ${seed}`)
   })
 
-  describe('balanced pool', function () {
-    // a balanced pool has two assets, both with the same tvl and 100% cov ratio.
+  describe('balanced pool with 100% cov ratio', function () {
     async function balancedPoolInvariant(tvl: BigNumberish, swapAmount: BigNumberish, ampFactor: BigNumberish) {
       return invariant(tvl, tvl, tvl, tvl, swapAmount, ampFactor)
     }
 
-    ;[100, 1e6, 1e9, 1e12, parseEther('1'), parseEther('5000'), parseEther('100000'), parseEther('1000000')].map(
-      (tvl) => {
-        it(`balanced pool (${tvl}) at equilibrium`, async function () {
-          // perform swap with 0~100% of TVL, plus 0~99 bips in each step.
-          await Promise.all(
-            _.range(0, 100).map((percent) => {
+    tvlList.map((tvl) => {
+      it(`balanced pool (${formatEther(tvl)}) at equilibrium`, async function () {
+        // perform swap with 0~100% of TVL, plus 0~99 bips in each step.
+        await Promise.all(
+          _.range(0, 100).map((percent) => {
+            const bips = random.int(0, 99) // [0, 100)
+            const swapAmount = BigNumber.from(tvl)
+              .mul(percent)
+              .div(100)
+              .mul(10000 + bips)
+              .div(10000)
+            // also add 0-99% randomness to tvl
+            const randomTvl = tvl.mul(100 + bips).div(100)
+            return balancedPoolInvariant(randomTvl, swapAmount, ampFactor)
+          })
+        )
+      })
+    })
+  })
+
+  describe('imbalanced pool with different tvl and 100% cov ratio', function () {
+    async function imbalancedPoolInvariant(
+      fromTvl: BigNumberish,
+      toTvl: BigNumberish,
+      swapAmount: BigNumberish,
+      ampFactor: BigNumberish
+    ) {
+      return Promise.all([
+        invariant(fromTvl, fromTvl, toTvl, toTvl, swapAmount, ampFactor),
+        invariant(toTvl, toTvl, fromTvl, fromTvl, swapAmount, ampFactor),
+      ])
+    }
+    tvlList.map((tvl) => {
+      it(`imbalanced pool with random TVL at equilibrium`, async function () {
+        // perform swap with 0~100% of TVL, plus 0~99 bips in each step.
+        await Promise.all(
+          // imbalance pool with 1-10x difference
+          _.range(0, 100).flatMap((percent) => {
+            return _.range(1, 10).map((ratio) => {
               const bips = random.int(0, 99) // [0, 100)
               const swapAmount = BigNumber.from(tvl)
                 .mul(percent)
                 .div(100)
                 .mul(10000 + bips)
                 .div(10000)
-              return balancedPoolInvariant(tvl, swapAmount, ampFactor)
+              const randomTvl = tvl.mul(100 + bips).div(100)
+              return imbalancedPoolInvariant(randomTvl, randomTvl.mul(ratio), swapAmount, ampFactor)
             })
-          )
-        })
-      }
-    )
+          })
+        )
+      })
+    })
   })
 
-  // TODO: set up pool with different TVL, 100% cov ratio
+  describe('balanced pool with 1-200% cov ratio', function () {
+    async function balancedPoolInvariantWithCov(
+      fromAssetCash: BigNumberish,
+      fromAssetLiability: BigNumberish,
+      toAssetCash: BigNumberish,
+      toAssetLiability: BigNumberish,
+      swapAmount: BigNumberish,
+      ampFactor: BigNumberish
+    ) {
+      return Promise.all([
+        invariant(fromAssetCash, fromAssetLiability, toAssetCash, toAssetLiability, swapAmount, ampFactor),
+        invariant(toAssetCash, toAssetLiability, fromAssetCash, fromAssetLiability, swapAmount, ampFactor),
+      ])
+    }
 
-  // TODO: set up pool with same TVL and 0~200% cov ratio
+    tvlList.map(async (tvl) => {
+      it(`balanced pool (${formatEther(tvl)}) with 1-200% cov ratio`, async function () {
+        // perform swap with 0~100% of TVL, plus 0~99 bips in each step.
+        await Promise.all(
+          _.range(1, 100).map((percent) => {
+            // random cov ratio from 1% ~ 200%
+            const fromCovRatio = random.int(100, 20000)
+            const toCovRatio = random.int(100, 20000)
+            const bips = random.int(0, 99) // [0, 100)
+            const swapAmount = BigNumber.from(tvl)
+              .mul(percent)
+              .div(100)
+              .mul(10000 + bips)
+              .div(10000)
+            const randomTvl = tvl.mul(100 + bips).div(100)
 
-  // TODO: set up pool with different TVL and different cov ratio
+            // same tvl, but different cash and cov ratio
+            const fromAssetCash = randomTvl.mul(fromCovRatio).div(10000)
+            const toAssetCash = randomTvl.mul(toCovRatio).div(10000)
+            return balancedPoolInvariantWithCov(fromAssetCash, randomTvl, toAssetCash, randomTvl, swapAmount, ampFactor)
+          })
+        )
+      })
+    })
+  })
+
+  describe('imbalanced pool with different tvl with 1-200% cov ratio', function () {
+    async function imbalancedPoolInvariantWithCov(
+      fromAssetCash: BigNumberish,
+      fromAssetLiability: BigNumberish,
+      toAssetCash: BigNumberish,
+      toAssetLiability: BigNumberish,
+      swapAmount: BigNumberish,
+      ampFactor: BigNumberish
+    ) {
+      return Promise.all([
+        invariant(fromAssetCash, fromAssetLiability, toAssetCash, toAssetLiability, swapAmount, ampFactor),
+        invariant(toAssetCash, toAssetLiability, fromAssetCash, fromAssetLiability, swapAmount, ampFactor),
+      ])
+    }
+
+    tvlList.map(async (tvl) => {
+      it(`imbalanced pool with different tvl and 1-200% cov ratio`, async function () {
+        // perform swap with 0~100% of TVL, plus 0~99 bips in each step.
+        await Promise.all(
+          // imbalance pool with 1-10x difference
+          _.range(1, 10).flatMap((ratio) => {
+            return _.range(1, 100).map((percent) => {
+              // random cov ratio from 1% ~ 200%
+              const fromCovRatio = random.int(100, 20000)
+              const toCovRatio = random.int(100, 20000)
+              const bips = random.int(0, 99) // [0, 100)
+              const swapAmount = BigNumber.from(tvl)
+                .mul(percent)
+                .div(100)
+                .mul(10000 + bips)
+                .div(10000)
+              // different tvl, cash and cov ratio
+              const randomFromTvl = tvl.mul(100 + bips).div(100)
+              const randomToTvl = randomFromTvl.mul(ratio)
+              const fromAssetCash = randomFromTvl.mul(fromCovRatio).div(10000)
+              const toAssetCash = randomToTvl.mul(toCovRatio).div(10000)
+              return imbalancedPoolInvariantWithCov(
+                fromAssetCash,
+                randomFromTvl,
+                toAssetCash,
+                randomToTvl,
+                swapAmount,
+                ampFactor
+              )
+            })
+          })
+        )
+      })
+    })
+  })
 
   // verify that quote = swapFromCredit(swapToCredit)
   async function invariant(
@@ -78,7 +209,7 @@ describe('CoreV3', function () {
       ampFactor,
     ].join(',')
     // expect absolute delta when quote is small; else relative.
-    const delta = quote.lt(parseEther('0.1')) ? 1 : quote.div(10000) // 1bps
+    const delta = quote.lt(parseEther('0.1')) ? 5 : quote.div(10000) // 1bps
     expect(quoteFromCredit, `invariant failed for (${parameters}) quoteFromCredit`).to.be.closeTo(quote, delta)
   }
 })
