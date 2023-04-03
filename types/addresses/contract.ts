4a8e9aa18dc8bc75395eacf580fb1a631b0ffbfc
jack <97011001+jack-wombat@users.noreply.github.com>
2023-04-02 21:32:00 -0700

    Rename to ExternalContract

--- types/addresses/contract.ts
+++ types/addresses/contract.ts
@@ -4,28 +4,28 @@ import { getAddress } from '../../utils'
 
 // Enum of external contract that are not tokens.
 // style note: sort alphabetically.
-export enum Contract {
+export enum ExternalContract {
   SkimAdmin,
   WombexVeWomProxy,
 }
 
-export async function getContractAddress(contract: Contract): Promise<string> {
+export async function getContractAddress(contract: ExternalContract): Promise<string> {
   const network = await getNetwork()
   const deploymentOrAddress = contractRegistry[contract][network]
   if (!deploymentOrAddress) {
-    throw new Error(`No config found for contract ${Contract[contract]} in network ${network}`)
+    throw new Error(`No config found for contract ${ExternalContract[contract]} in network ${network}`)
   } else {
     return getAddress(deploymentOrAddress)
   }
 }
 
 // registry of contract address. Used by getContractAddress only. Do not export.
-const contractRegistry: Record<Contract, PartialRecord<Network, DeploymentOrAddress>> = {
-  [Contract.SkimAdmin]: {
+const contractRegistry: Record<ExternalContract, PartialRecord<Network, DeploymentOrAddress>> = {
+  [ExternalContract.SkimAdmin]: {
     [Network.BSC_MAINNET]: Address('0xD9fCDFFEd5cA34Ef21661Ec6Fe3AEb742db6331e'),
     [Network.ARBITRUM_MAINNET]: Address('0x145F2a1aa70098031629606d856591dA0C717554'),
   },
-  [Contract.WombexVeWomProxy]: {
+  [ExternalContract.WombexVeWomProxy]: {
     // https://arbiscan.io/address/0x24D2f6be2bF9cdf3627f720cf09D4551580C1eC1
     [Network.ARBITRUM_MAINNET]: Address('0x24D2f6be2bF9cdf3627f720cf09D4551580C1eC1'),
   },
