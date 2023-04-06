e1aab6a0e1077305c3d5d3eca163ba951df27a9d
jack <97011001+jack-wombat@users.noreply.github.com>
2023-04-06 09:08:58 -0700

    Increase MultiRewarderPerSec token precision to 1e18
    Increase token precision to 1e18 to handle token with lower decimals
    (USDC/USD+ only has 6).
    
    Able to get accurate amount in test case. And no overflow is found at
    max reward rate for one month.

--- test/Rewarder.spec.ts
+++ test/Rewarder.spec.ts
@@ -1,4 +1,4 @@
-import { BigNumberish, Contract } from 'ethers'
+import { BigNumber, BigNumberish, Contract } from 'ethers'
 import { deployments, ethers } from 'hardhat'
 import { getTestERC20 } from '../utils'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
@@ -9,6 +9,7 @@ import { expect } from 'chai'
 describe('[Rewarder]', function () {
   const vUSDCDecimals = 8
   const USDCDecimals = 18
+  const maxTokenPerSec = parseEther('10000')
 
   let owner: SignerWithAddress
   let user: SignerWithAddress
@@ -28,58 +29,78 @@ describe('[Rewarder]', function () {
     expect(await vUSDC.decimals()).to.eq(vUSDCDecimals)
 
     // Note: rewarder use master's LP balance as total share
-    await USDC.faucet(parseUSDC('1'))
-    await USDC.transfer(master.address, parseUSDC('1'))
+    await topUp(master.address, parseUSDC('1'))
   })
 
   describe('vUSDC (8 decimal)', function () {
     it('rewards 1 vUSDC/s', async function () {
-      const vUSDCRewarder = await deployRewarder({
+      const rewarder = await deployRewarder({
         rewardToken: vUSDC.address,
         tokenPerSec: parseVUSDC('1'),
       })
-      await master.onReward(vUSDCRewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
       await time.increase(3600) // T+1h
-      const [rewards] = await vUSDCRewarder.pendingTokens(user.address)
+      const [rewards] = await rewarder.pendingTokens(user.address)
       expect(rewards).to.eql(parseVUSDC('3600'))
     })
 
-    it('does not work with small shares', async function () {
-      const vUSDCRewarder = await deployRewarder({
+    it('works with small shares', async function () {
+      const rewarder = await deployRewarder({
         rewardToken: vUSDC.address,
         tokenPerSec: parseVUSDC('1'),
       })
-      await USDC.faucet(parseUSDC('999999'))
-      await USDC.transfer(master.address, parseUSDC('999999'))
+      await topUpTo(master.address, parseUSDC('1000000'))
       expect(await USDC.balanceOf(master.address)).to.eql(parseUSDC('1000000'))
 
-      await master.onReward(vUSDCRewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
       await time.increase(3600) // T+1h
       {
-        const [rewards] = await vUSDCRewarder.pendingTokens(user.address)
-        // Technically, we should get 0.0036 = (3600 * 1 * 1 / 100000). However, we lose precision.
-        expect(rewards).to.eql(parseVUSDC('0'))
+        const [rewards] = await rewarder.pendingTokens(user.address)
+        expect(rewards).to.eql(parseVUSDC('0.0036'))
       }
 
       await time.increase((7 * 24 - 1) * 3600) // T+7d
       {
-        const [rewards] = await vUSDCRewarder.pendingTokens(user.address)
-        // (7 * 24 * 3600 * 1 * 1 / 1000000) = 0.6048
-        expect(rewards).to.eql(parseVUSDC('0.6'))
+        const [rewards] = await rewarder.pendingTokens(user.address)
+        expect(rewards).to.eql(parseVUSDC('0.6048'))
       }
+    })
+
+    it('does not overflow at max reward rate for one month', async function () {
+      const rewarder = await deployRewarder({
+        rewardToken: vUSDC.address,
+        tokenPerSec: maxTokenPerSec,
+      })
+      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await time.increase(30 * 24 * 3600) // T+30d
+      const [rewards] = await rewarder.pendingTokens(user.address)
+      expect(rewards).to.eql(parseVUSDC('259200000000000000000'))
+      expect(rewards).to.eql(maxTokenPerSec.mul(30 * 24 * 3600))
     })
   })
 
   describe('USDC (18 decimal)', function () {
     it('rewards 1 USDC/s', async function () {
-      const USDCRewarder = await deployRewarder({
+      const rewarder = await deployRewarder({
         rewardToken: USDC.address,
         tokenPerSec: parseUSDC('1'),
       })
-      await master.onReward(USDCRewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
       await time.increase(3600) // T+1h
-      const [rewards] = await USDCRewarder.pendingTokens(user.address)
+      const [rewards] = await rewarder.pendingTokens(user.address)
       expect(rewards).to.eql(parseUSDC('3600'))
+    })
+
+    it('does not overflow at max reward rate for one month', async function () {
+      const rewarder = await deployRewarder({
+        rewardToken: USDC.address,
+        tokenPerSec: maxTokenPerSec,
+      })
+      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await time.increase(30 * 24 * 3600) // T+30d
+      const [rewards] = await rewarder.pendingTokens(user.address)
+      expect(rewards).to.eql(parseUSDC('25920000000'))
+      expect(rewards).to.eql(maxTokenPerSec.mul(30 * 24 * 3600))
     })
   })
 
@@ -97,6 +118,16 @@ describe('[Rewarder]', function () {
       /*rewardToken=*/ rewardInfo.rewardToken,
       /*tokenPerSec=*/ rewardInfo.tokenPerSec, // must match rewardToken decimals!
     ])
+  }
+
+  async function topUp(address: string, amount: BigNumberish) {
+    await USDC.faucet(amount)
+    await USDC.transfer(address, amount)
+  }
+
+  async function topUpTo(address: string, target: BigNumberish) {
+    const amount = BigNumber.from(target).sub(await USDC.balanceOf(address))
+    await topUp(address, amount)
   }
 
   function parseUSDC(value: string) {
