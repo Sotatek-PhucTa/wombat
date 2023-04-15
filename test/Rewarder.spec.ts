8b5d27bcf42b8452339ac84170c921c9a7a25ffb
jack <97011001+jack-wombat@users.noreply.github.com>
2023-04-15 15:13:57 -0700

    Reproduce bug to take all bribe
    This happened on the dev account for MIM USDT bribe. Here is a repro
    proving how that can happen. Fix will be send in a separate PR.

--- test/Rewarder.spec.ts
+++ test/Rewarder.spec.ts
@@ -4,17 +4,19 @@ import { getTestERC20 } from '../utils'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { time } from '@nomicfoundation/hardhat-network-helpers'
 import { parseEther, parseUnits } from 'ethers/lib/utils'
-import { expect } from 'chai'
+import { assert, expect } from 'chai'
 
 describe('[Rewarder]', function () {
-  const axlUSDCDecimals = 6
   const USDCDecimals = 18
+  const DAIDecimals = 18
+  const axlUSDCDecimals = 6
   const maxTokenPerSec = parseEther('10000')
 
   let owner: SignerWithAddress
   let user: SignerWithAddress
   let master: Contract
   let USDC: Contract
+  let DAI: Contract
   let axlUSDC: Contract
 
   beforeEach(async function () {
@@ -25,11 +27,17 @@ describe('[Rewarder]', function () {
     USDC = await getTestERC20('USDC')
     expect(await USDC.decimals()).to.eq(USDCDecimals)
 
+    DAI = await getTestERC20('DAI')
+    expect(await DAI.decimals()).to.eq(DAIDecimals)
+
     axlUSDC = await getTestERC20('axlUSDC')
     expect(await axlUSDC.decimals()).to.eq(axlUSDCDecimals)
 
-    // Note: rewarder use master's LP balance as total share
-    await topUp(master.address, parseUSDC('1'))
+    await Promise.all(
+      [USDC, DAI, axlUSDC].map(async (token) => {
+        token.approve(master.address, ethers.constants.MaxUint256)
+      })
+    )
   })
 
   describe('axlUSDC (6 decimal)', function () {
@@ -38,10 +46,10 @@ describe('[Rewarder]', function () {
         rewardToken: axlUSDC.address,
         tokenPerSec: parseAxlUSDC('1'),
       })
-      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await deposit(user, master, rewarder, parseEther('1'))
       await time.increase(3600) // T+1h
       const [rewards] = await rewarder.pendingTokens(user.address)
-      expect(rewards).to.eql(parseAxlUSDC('3600'))
+      expect(rewards).to.be.closeTo(parseAxlUSDC('3600'), parseAxlUSDC('3'))
     })
 
     it('works with small shares', async function () {
@@ -49,9 +57,7 @@ describe('[Rewarder]', function () {
         rewardToken: axlUSDC.address,
         tokenPerSec: parseAxlUSDC('1'),
       })
-      await topUpTo(master.address, parseUSDC('1000000'))
-      expect(await USDC.balanceOf(master.address)).to.eql(parseUSDC('1000000'))
-
+      await deposit(owner, master, rewarder, parseEther('1000000'))
       await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
       const now = await time.latest()
       await time.increaseTo(now + 1) // T+1s
@@ -78,11 +84,10 @@ describe('[Rewarder]', function () {
         rewardToken: axlUSDC.address,
         tokenPerSec: maxTokenPerSec,
       })
-      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await deposit(user, master, rewarder, parseEther('1'))
       await time.increase(30 * 24 * 3600) // T+30d
       const [rewards] = await rewarder.pendingTokens(user.address)
-      expect(rewards).to.eql(parseAxlUSDC('25920000000000000000000'))
-      expect(rewards).to.eql(maxTokenPerSec.mul(30 * 24 * 3600))
+      expect(rewards).to.be.closeTo(maxTokenPerSec.mul(30 * 24 * 3600), maxTokenPerSec.mul(3))
     })
   })
 
@@ -92,10 +97,10 @@ describe('[Rewarder]', function () {
         rewardToken: USDC.address,
         tokenPerSec: parseUSDC('1'),
       })
-      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await deposit(user, master, rewarder, parseEther('1'))
       await time.increase(3600) // T+1h
       const [rewards] = await rewarder.pendingTokens(user.address)
-      expect(rewards).to.eql(parseUSDC('3600'))
+      expect(rewards).to.be.closeTo(parseUSDC('3600'), parseUSDC('3'))
     })
 
     it('does not overflow at max reward rate for one month', async function () {
@@ -103,42 +108,110 @@ describe('[Rewarder]', function () {
         rewardToken: USDC.address,
         tokenPerSec: maxTokenPerSec,
       })
-      await master.onReward(rewarder.address, user.address, /*lpAmount=*/ parseEther('1'))
+      await deposit(user, master, rewarder, parseEther('1'))
       await time.increase(30 * 24 * 3600) // T+30d
       const [rewards] = await rewarder.pendingTokens(user.address)
-      expect(rewards).to.eql(parseUSDC('25920000000'))
-      expect(rewards).to.eql(maxTokenPerSec.mul(30 * 24 * 3600))
+      expect(rewards).to.be.closeTo(maxTokenPerSec.mul(30 * 24 * 3600), maxTokenPerSec.mul(3))
+    })
+  })
+
+  describe('Emission', function () {
+    let rewarder: Contract
+
+    beforeEach(async function () {
+      rewarder = await deployRewarder({
+        rewardToken: DAI.address,
+        tokenPerSec: parseDAI('0'),
+        lpToken: DAI.address,
+      })
+    })
+
+    it('UpdateReward does not crash when there are no shares', async function () {
+      // This could crash if there is a division by zero.
+      expect(await getTotalShares(rewarder)).to.eq(0)
+      expect(await rewarder.updateReward()).to.be.ok
+    })
+
+    it('A user can steal all bribes', async function () {
+      // A rewarder is deployed for next bribe a long time ago
+      // A user deposits and withdraw some LP token when there are no rewards
+      await deposit(user, master, rewarder, parseDAI('1'))
+      await withdraw(user, master, rewarder, parseDAI('1'))
+      expect(await getTotalShares(rewarder)).to.eq(0)
+      const lastRewardTimestamp = await rewarder.lastRewardTimestamp()
+
+      // Two weeks later, we activate the rewarder at 1 DAI/s
+      await time.increase(14 * 24 * 3600) // T+14d
+      await rewarder.setRewardRate(0, parseDAI('1'))
+      // Timestamp is not updated because there are no shares
+      expect(await rewarder.lastRewardTimestamp()).to.eq(lastRewardTimestamp)
+
+      // The user deposits again after the reward is live.
+      // And takes all the bribe even though its only a second.
+      await deposit(user, master, rewarder, parseDAI('1'))
+      const [rewards] = await rewarder.pendingTokens(user.address)
+      expect(rewards).to.be.closeTo(parseDAI('1').mul(14 * 24 * 3600), parseDAI('4'))
     })
   })
 
   interface RewardInfo {
     rewardToken: string
     tokenPerSec: BigNumberish
+    lpToken?: string
   }
 
   async function deployRewarder(rewardInfo: RewardInfo) {
     return ethers.deployContract('MultiRewarderPerSec', [
       /*master=*/ master.address,
       // lpToken must have 18 decimals!
-      /*lpToken=*/ USDC.address,
-      (await time.latest()) + 1,
+      /*lpToken=*/ rewardInfo.lpToken ?? USDC.address,
+      /*startTimestamp=*/ (await time.latest()) + 1, // starts immediately
       /*rewardToken=*/ rewardInfo.rewardToken,
       /*tokenPerSec=*/ rewardInfo.tokenPerSec, // must match rewardToken decimals!
     ])
   }
 
-  async function topUp(address: string, amount: BigNumberish) {
-    await USDC.faucet(amount)
-    await USDC.transfer(address, amount)
+  // Simulate a deposit by increase LP balance in master and calls onReward.
+  async function deposit(user: SignerWithAddress, master: Contract, rewarder: Contract, amount: BigNumberish) {
+    const erc20 = await getLPTokenAsTestERC20(rewarder)
+    await topUp(owner.address, amount, erc20)
+    return master.depositFor(rewarder.address, user.address, amount)
   }
 
-  async function topUpTo(address: string, target: BigNumberish) {
-    const amount = BigNumber.from(target).sub(await USDC.balanceOf(address))
-    await topUp(address, amount)
+  // Simulate a withdraw by decreasing LP balance in master and calls onReward.
+  async function withdraw(user: SignerWithAddress, master: Contract, rewarder: Contract, amount: BigNumberish) {
+    return master.withdrawFor(rewarder.address, user.address, amount)
+  }
+
+  async function topUp(address: string, amount: BigNumberish, erc20: Contract = USDC) {
+    await erc20.faucet(amount)
+    return erc20.transfer(address, amount)
+  }
+
+  async function topUpTo(address: string, target: BigNumberish, erc20: Contract = USDC) {
+    const amount = BigNumber.from(target).sub(await erc20.balanceOf(address))
+    assert(amount.gt(0), 'Cannot topUp negative amount')
+    return topUp(address, amount)
+  }
+
+  // Rewarder use master's LP balance as total share.
+  async function getTotalShares(rewarder: Contract) {
+    const erc20 = await getLPTokenAsTestERC20(rewarder)
+    const master = await rewarder.master()
+    return erc20.balanceOf(master)
+  }
+
+  async function getLPTokenAsTestERC20(rewarder: Contract) {
+    const lpToken = await rewarder.lpToken()
+    return ethers.getContractAt('TestERC20', lpToken)
   }
 
   function parseUSDC(value: string) {
     return parseUnits(value, USDCDecimals)
+  }
+
+  function parseDAI(value: string) {
+    return parseUnits(value, DAIDecimals)
   }
 
   function parseAxlUSDC(value: string) {
