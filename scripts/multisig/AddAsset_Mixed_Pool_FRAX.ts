7dfddb8978369bcf81c8fbce1f52dee7112e48da
jack <97011001+jack-wombat@users.noreply.github.com>
2023-05-08 09:53:53 -0700

    Fix AddAsset script

--- scripts/multisig/AddAsset_Mixed_Pool_FRAX.ts
+++ scripts/multisig/AddAsset_Mixed_Pool_FRAX.ts
@@ -4,7 +4,7 @@ import * as multisig from '../../utils/multisig'
 
 runScript('AddAsset_Mixed_Pool_FRAX', async () => {
   return concatAll(
-    multisig.utils.addAssetToPool('Asset_Mixed_Pool_FRAX', 'FactoryPools_MixedPool_Proxy'),
+    multisig.utils.addAssetToPool('Asset_Mixed_Pool_FRAX', 'FactoryPools_Mixed_Pool_Proxy'),
     multisig.utils.addAssetToMasterWombatAndVoter('Asset_Mixed_Pool_FRAX')
   )
 })
