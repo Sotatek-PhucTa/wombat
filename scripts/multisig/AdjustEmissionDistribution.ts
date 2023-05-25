acd61112ced7b30679d5d6d8e78d17de640c305b
tj-wombat <shinji@wombat.exchange>
2023-05-25 17:29:18 +0800

    update emission rate and assets rename

--- scripts/multisig/AdjustEmissionDistribution.ts
+++ scripts/multisig/AdjustEmissionDistribution.ts
@@ -19,13 +19,13 @@ runScript('AdjustEmissionDistribution', async () => {
   let GAUGE_ALLOC_PERCENTS
 
   if (network == Network.ARBITRUM_MAINNET) {
-    WOM_MONTHLY_EMISSION_RATE = 1700000
-    BRIBE_ALLOC_PERCENT = 45
+    WOM_MONTHLY_EMISSION_RATE = 1200000
+    BRIBE_ALLOC_PERCENT = 60
     GAUGE_ALLOC_PERCENTS = {
       // Main Pool
-      Asset_P01_USDC: 26.25,
-      Asset_P01_USDT: 20.6,
-      Asset_P01_DAI: 5.65,
+      Asset_MainPool_USDC: 18.75,
+      Asset_MainPool_USDT: 14.7,
+      Asset_MainPool_DAI: 4.05,
       // wmxWOM Pool
       Asset_wmxWOM_Pool_wmxWOM: 0.6,
       Asset_wmxWOM_Pool_WOM: 0.6,
@@ -37,16 +37,16 @@ runScript('AdjustEmissionDistribution', async () => {
       Asset_qWOM_Pool_WOM: 0.2,
     }
   } else if (network == Network.BSC_MAINNET) {
-    WOM_MONTHLY_EMISSION_RATE = 2000000
-    BRIBE_ALLOC_PERCENT = 62.5
+    WOM_MONTHLY_EMISSION_RATE = 1500000
+    BRIBE_ALLOC_PERCENT = 70
     GAUGE_ALLOC_PERCENTS = {
       // Main Pool
-      Asset_P01_USDC: 15.5,
-      Asset_P01_USDT: 15.5,
-      Asset_P01_DAI: 2,
+      Asset_MainPool_USDC: 11.7,
+      Asset_MainPool_USDT: 11.7,
+      Asset_MainPool_DAI: 1.6,
       // TODO: standardize pool names. i.e. Asset_wmxWOMPool_wmxWOM is used here but for bsc it is Asset_wmxWOM_Pool_wmxWOM
       // wmxWOM Pool
-      Asset_wmxWOMPool_wmxWOM: 0.6,
+      Asset_wmxWOMPool_wmxWom: 0.6,
       Asset_wmxWOMPool_WOM: 0.6,
       // mWOM Pool
       Asset_mWOMPool_mWOM: 0.45,
@@ -55,8 +55,8 @@ runScript('AdjustEmissionDistribution', async () => {
       Asset_qWOMPool_qWOM: 0.2,
       Asset_qWOMPool_WOM: 0.2,
       // wBETH Pool
-      Asset_wBETH_Pool_wBETH: 1.5,
-      Asset_wBETH_Pool_ETH: 0.5,
+      Asset_wBETH_Pool_wBETH: 1.75,
+      Asset_wBETH_Pool_ETH: 0.75,
     }
   }
 
