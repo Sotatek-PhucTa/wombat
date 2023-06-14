35f41392af5419a10af3de5b7918a3e6eeaf9fa2
jack <97011001+jack-wombat@users.noreply.github.com>
2023-06-13 17:04:02 -0700

    [BSC] Timelock phrase 1: Bnbx + ProxyAdmin

--- scripts/multisig/Timelock_TransferOwnership.ts
+++ scripts/multisig/Timelock_TransferOwnership.ts
@@ -1,12 +1,18 @@
+import assert from 'assert'
 import { runScript } from '.'
-import { Deployment } from '../../types'
+import { Deployment, Network } from '../../types'
+import { getCurrentNetwork } from '../../types/network'
 import { concatAll } from '../../utils'
-import { getAllAssetsDeployments } from '../../utils/deploy'
 import * as multisig from '../../utils/multisig'
 
 runScript('Timelock_TransferOwnership', async () => {
+  assert((await getCurrentNetwork()) == Network.BSC_MAINNET, 'Only bsc mainnet is supported.')
   return concatAll(
-    multisig.utils.transferAssetsOwnership(await getAllAssetsDeployments(), Deployment('TimelockController')),
+    multisig.utils.transferAssetsOwnership(
+      ['Asset_BnbxPool_BNBx', 'Asset_BnbxPool_WBNB'],
+      // TODO: add main pool after verification
+      Deployment('TimelockController')
+    ),
     multisig.utils.transferProxyAdminOwnership(Deployment('TimelockController'))
   )
 })
