112f68ab7075f063c7980c8eb9916c2d6420f045
jack <97011001+jack-wombat@users.noreply.github.com>
2023-06-20 15:11:28 -0700

    Introduce wormhole.config.ts

--- config/tokens.config.ts
+++ config/tokens.config.ts
@@ -1,16 +1,6 @@
 import { BigNumber } from 'ethers'
 import { parseEther, parseUnits } from 'ethers/lib/utils'
-import {
-  Deployment,
-  IGovernedPriceFeed,
-  IHighCovRatioFeePoolConfig,
-  IWormholeAdaptorConfig,
-  IWormholeConfig,
-  Network,
-  NetworkPoolInfo,
-  PartialRecord,
-  PoolName,
-} from '../types'
+import { IGovernedPriceFeed, IHighCovRatioFeePoolConfig, Network, NetworkPoolInfo, PartialRecord } from '../types'
 import {
   AnkrBnbAsset,
   AnkrEthAsset,
@@ -135,6 +125,7 @@ const defaultCrossChainPoolConfig: IHighCovRatioFeePoolConfig = {
   supportNativeToken: false,
 }
 
+// @deprecated: Please expose the value type directly using getCurrentNetwork().
 // inject forkNetwork to hardhat and localhost
 export function injectForkNetwork<T>(config: PartialRecord<Network, T>): PartialRecord<Network, T> {
   const forkNetwork = process.env.FORK_NETWORK || ''
@@ -771,29 +762,6 @@ export const VOLATILEPOOL_TOKENS_MAP: PartialRecord<
   },
 })
 
-export const WORMHOLE_CONFIG_MAPS: PartialRecord<Network, IWormholeConfig> = injectForkNetwork<IWormholeConfig>({
-  [Network.BSC_TESTNET]: {
-    relayer: '0xda2592C43f2e10cBBA101464326fb132eFD8cB09',
-    wormholeBridge: '0x68605AD7b15c732a30b1BbC62BE8F2A509D74b4D',
-    consistencyLevel: 15,
-  },
-  [Network.AVALANCHE_TESTNET]: {
-    relayer: '0xDDe6b89B7d0AD383FafDe6477f0d300eC4d4033e',
-    wormholeBridge: '0x7bbcE28e64B3F8b84d876Ab298393c38ad7aac4C',
-    consistencyLevel: 1,
-  },
-  [Network.LOCALHOST]: {
-    relayer: '0x0000000000000000000000000000000000000000',
-    wormholeBridge: '0x0000000000000000000000000000000000000000',
-    consistencyLevel: 1,
-  },
-  [Network.HARDHAT]: {
-    relayer: '0x0000000000000000000000000000000000000000',
-    wormholeBridge: '0x0000000000000000000000000000000000000000',
-    consistencyLevel: 1,
-  },
-})
-
 export const CROSS_CHAIN_POOL_TOKENS_MAP: PartialRecord<
   Network,
   NetworkPoolInfo<IHighCovRatioFeePoolConfig>
@@ -818,24 +786,6 @@ export const CROSS_CHAIN_POOL_TOKENS_MAP: PartialRecord<
         ...BusdAsset(),
         ...VusdcAsset(),
       },
-    },
-  },
-})
-
-export const WORMHOLE_ADAPTOR_CONFIG_MAP: PartialRecord<
-  Network,
-  Record<PoolName, IWormholeAdaptorConfig>
-> = injectForkNetwork<Record<PoolName, IWormholeAdaptorConfig>>({
-  [Network.BSC_TESTNET]: {
-    stablecoinPool: {
-      adaptorAddr: Deployment('WormholeAdaptor_stablecoinPool_Proxy'),
-      tokens: [Token.BUSD, Token.vUSDC],
-    },
-  },
-  [Network.AVALANCHE_TESTNET]: {
-    stablecoinPool: {
-      adaptorAddr: Deployment('WormholeAdaptor_stablecoinPool_Proxy'),
-      tokens: [Token.BUSD, Token.vUSDC],
     },
   },
 })
---
+++ config/wormhole.config.ts
@@ -0,0 +1,44 @@
+import { Deployment, IWormholeAdaptorConfig, IWormholeConfig, Network, PartialRecord, PoolName } from '../types'
+import { Token } from './token'
+import { injectForkNetwork } from './tokens.config'
+
+export const WORMHOLE_CONFIG_MAPS: PartialRecord<Network, IWormholeConfig> = injectForkNetwork<IWormholeConfig>({
+  [Network.BSC_TESTNET]: {
+    relayer: '0xda2592C43f2e10cBBA101464326fb132eFD8cB09',
+    wormholeBridge: '0x68605AD7b15c732a30b1BbC62BE8F2A509D74b4D',
+    consistencyLevel: 15,
+  },
+  [Network.AVALANCHE_TESTNET]: {
+    relayer: '0xDDe6b89B7d0AD383FafDe6477f0d300eC4d4033e',
+    wormholeBridge: '0x7bbcE28e64B3F8b84d876Ab298393c38ad7aac4C',
+    consistencyLevel: 1,
+  },
+  [Network.LOCALHOST]: {
+    relayer: '0x0000000000000000000000000000000000000000',
+    wormholeBridge: '0x0000000000000000000000000000000000000000',
+    consistencyLevel: 1,
+  },
+  [Network.HARDHAT]: {
+    relayer: '0x0000000000000000000000000000000000000000',
+    wormholeBridge: '0x0000000000000000000000000000000000000000',
+    consistencyLevel: 1,
+  },
+})
+
+export const WORMHOLE_ADAPTOR_CONFIG_MAP: PartialRecord<
+  Network,
+  Record<PoolName, IWormholeAdaptorConfig>
+> = injectForkNetwork<Record<PoolName, IWormholeAdaptorConfig>>({
+  [Network.BSC_TESTNET]: {
+    stablecoinPool: {
+      adaptorAddr: Deployment('WormholeAdaptor_stablecoinPool_Proxy'),
+      tokens: [Token.BUSD, Token.vUSDC],
+    },
+  },
+  [Network.AVALANCHE_TESTNET]: {
+    stablecoinPool: {
+      adaptorAddr: Deployment('WormholeAdaptor_stablecoinPool_Proxy'),
+      tokens: [Token.BUSD, Token.vUSDC],
+    },
+  },
+})
