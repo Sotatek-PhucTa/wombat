ae7c3e4597f16a4a42157c07ccabe423f5f1edbd
jack <97011001+jack-wombat@users.noreply.github.com>
2023-06-26 22:31:54 -0700

    mPendle pool setRewarder

--- scripts/multisig/AddAsset_mPendle_Pool.ts
+++ scripts/multisig/AddAsset_mPendle_Pool.ts
@@ -1,11 +1,20 @@
 import { runScript } from '.'
 import { concatAll } from '../../utils'
+import { getRewarderDeploymentName } from '../../utils/deploy'
 import * as multisig from '../../utils/multisig'
 
 runScript('AddAssets_mPendle_Pool', async () => {
   return concatAll(
     ...['Asset_mPendle_Pool_mPendle', 'Asset_mPendle_Pool_PENDLE'].map((assetDeployment) =>
       multisig.utils.addAssetToMasterWombatAndVoter(assetDeployment)
+    )
+  )
+})
+
+runScript('SetRewarder_mPendle_Pool', async () => {
+  return concatAll(
+    ...['Asset_mPendle_Pool_mPendle', 'Asset_mPendle_Pool_PENDLE'].map((assetDeployment) =>
+      multisig.utils.setRewarder(getRewarderDeploymentName(assetDeployment))
     )
   )
 })
