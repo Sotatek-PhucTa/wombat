f574f17f54d62fe15e867ae711760f302956742d
tj-wombat <shinji@wombat.exchange>
2023-07-07 17:43:24 +0800

    genearte proposal

--- scripts/multisig/SetMaxSupply.ts
+++ scripts/multisig/SetMaxSupply.ts
@@ -1,10 +1,17 @@
 import { parseEther } from 'ethers/lib/utils'
 import { runScript } from '.'
 import * as multisig from '../../utils/multisig'
+import { Network } from '../../types'
+import { getCurrentNetwork } from '../../types/network'
 
 runScript('SetMaxSupply', async () => {
-  return Promise.all([
-    multisig.utils.setMaxSupply('Asset_WstETH_Pool_wstETH', parseEther('3200')),
-    // multisig.utils.setMaxSupply('Asset_WstETH_Pool_WETH', parseEther('3200')),
-  ])
+  const network = getCurrentNetwork()
+  if (network === Network.ARBITRUM_MAINNET) {
+    return Promise.all([
+      multisig.utils.setMaxSupply('Asset_WstETH_Pool_wstETH', parseEther('3200')),
+      // multisig.utils.setMaxSupply('Asset_WstETH_Pool_WETH', parseEther('3200')),
+    ])
+  } else {
+    throw new Error('Wrong network')
+  }
 })
