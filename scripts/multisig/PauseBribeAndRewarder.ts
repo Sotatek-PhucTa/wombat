269ca7717c4874ec5ab80027f38d9c06d9650976
tj-wombat <129966081+tj-wombat@users.noreply.github.com>
2023-07-18 16:31:26 +0800

    Chore/pause bribe 20230717 (#696)
    * rename bribe frax deployment file
    
    * proposal script

--- scripts/multisig/PauseBribeAndRewarder.ts
+++ scripts/multisig/PauseBribeAndRewarder.ts
@@ -8,12 +8,26 @@ runScript('PauseBribeAndRewarder', async () => {
   const network = getCurrentNetwork()
   if (network === Network.ARBITRUM_MAINNET) {
     return concatAll(
-      multisig.utils.pauseRewardRateFor('Bribe', ['Asset_WstETH_Pool_wstETH']),
-      multisig.utils.pauseRewardRateFor('Rewarder', ['Asset_WstETH_Pool_WETH', 'Asset_WstETH_Pool_wstETH']),
-      multisig.utils.pauseVoteEmissionFor(['Asset_WstETH_Pool_WETH', 'Asset_WstETH_Pool_wstETH'])
+      multisig.utils.pauseRewardRateFor('Bribe', [
+        'Asset_ankrETH_Pool_ankrETH',
+        'Asset_ankrETH_Pool_WETH',
+        'Asset_frxETH_Pool_frxETH',
+        'Asset_frxETH_Pool_WETH',
+        'Asset_frxETH_Pool_sfrxETH',
+      ])
     )
   } else if (network == Network.BSC_MAINNET) {
-    return concatAll(multisig.utils.pauseRewardRateFor('Rewarder', ['Asset_BNBy_Pool_WBNB', 'Asset_BNBy_Pool_BNBy']))
+    return concatAll(
+      multisig.utils.pauseRewardRateFor('Bribe', [
+        'Asset_stables_01_FRAX',
+        'Asset_Mixed_Pool_USDC',
+        'Asset_ankrETH_Pool_ETH',
+        'Asset_ankrETH_Pool_ankrETH',
+        'Asset_AnkrBNBPool_WBNB',
+        'Asset_AnkrBNBPool_ankrBNB',
+      ]),
+      multisig.utils.pauseRewardRateFor('Rewarder', ['Asset_stables_01_FRAX'])
+    )
   } else {
     throw new Error('Wrong network')
   }
