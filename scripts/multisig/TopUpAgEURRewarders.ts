9c6131be0d8243b61268aa009036aa9149048dce
tj-wombat <shinji@wombat.exchange>
2023-08-04 18:13:37 +0800

    update script

--- scripts/multisig/TopUpAgEURRewarders.ts
+++ scripts/multisig/TopUpAgEURRewarders.ts
@@ -9,18 +9,16 @@ import { BatchTransaction } from '../../utils/multisig/tx-builder'
 import { getRewarderDeploymentName } from '../../utils/deploy'
 import { convertTokenPerSecToTokenPerEpoch } from '../../config/emission'
 
-runScript('TopUpAllRewarders', async function (): Promise<BatchTransaction[]> {
+runScript('TopUpAgEURRewarders', async function (): Promise<BatchTransaction[]> {
   const network: Network = getCurrentNetwork()
   console.log(`Running against network: ${network}`)
   if (network == Network.ETHEREUM_MAINNET) {
-    // Note: they have not topped up ANGLE token yet.
-    const toPause = ['Asset_agEUR_Pool_EURe', 'Asset_agEUR_Pool_agEUR']
+    const assetsToTopUp = ['Asset_agEUR_Pool_EURe', 'Asset_agEUR_Pool_agEUR']
 
     const txns: BatchTransaction[] = []
-    for await (const [asset, config] of Object.entries(getRewarders())) {
-      if (toPause.includes(asset)) {
-        continue
-      }
+    for await (const [asset, config] of Object.entries(getRewarders()).filter(([asset, _]) =>
+      assetsToTopUp.includes(asset)
+    )) {
       for await (const [token, tokenPerSec] of _.zip(config.rewardTokens, config.tokenPerSec)) {
         assert(token != undefined && tokenPerSec != undefined)
         txns.push(
@@ -32,7 +30,7 @@ runScript('TopUpAllRewarders', async function (): Promise<BatchTransaction[]> {
         )
       }
     }
-    return txns.concat(await multisig.utils.pauseRewardRateFor('Rewarder', toPause))
+    return txns
   } else {
     return []
   }
