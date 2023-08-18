982a56d9693026e77e40135a9dabc614192feb79
berlinp-wombat <berlin@wombat.exchange>
2023-08-18 09:30:12 +0700

    Using deployBasePool to deploy CrosschainPool

--- deploy/060_CrossChainPool.ts
+++ deploy/060_CrossChainPool.ts
@@ -1,17 +1,16 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { Contract } from 'ethers'
-import { deployments, ethers, getNamedAccounts, network, upgrades } from 'hardhat'
+import { deployments, ethers, getNamedAccounts } from 'hardhat'
 import { CROSS_CHAIN_POOL_TOKENS_MAP } from '../config/pools.config'
-import { confirmTxn, getDeployedContract, logVerifyCommand } from '../utils'
-import { getPoolDeploymentName } from '../utils/deploy'
+import { confirmTxn, getDeployedContract } from '../utils'
+import { deployBasePool } from '../utils/deploy'
 import { getCurrentNetwork } from '../types/network'
 import { ICrossChainPoolConfig } from '../types'
 
 export const contractNamePrefix = 'CrossChainPool'
 
 const deployFunc = async function () {
-  const { deploy } = deployments
-  const { deployer, multisig } = await getNamedAccounts()
+  const { deployer } = await getNamedAccounts()
   const network = getCurrentNetwork()
   const deployerSigner = await SignerWithAddress.create(ethers.provider.getSigner(deployer))
   const coreV3Deployment = await getDeployedContract('CoreV3')
@@ -20,77 +19,15 @@ const deployFunc = async function () {
   /// Deploy pool
   const CROSS_CHAIN_POOL_TOKENS = CROSS_CHAIN_POOL_TOKENS_MAP[network] || {}
   for (const [poolName, poolInfo] of Object.entries(CROSS_CHAIN_POOL_TOKENS)) {
-    const contractName = getPoolDeploymentName(contractNamePrefix, poolName)
-    const deployResult = await deploy(contractName, {
-      from: deployer,
-      log: true,
-      contract: 'CrossChainPool',
-      skipIfAlreadyDeployed: true,
-      libraries: { CoreV3: coreV3Deployment.address },
-      proxy: {
-        owner: multisig, // change to Gnosis Safe after all admin scripts are done
-        proxyContract: 'OptimizedTransparentProxy',
-        viaAdminContract: 'DefaultProxyAdmin',
-        execute: {
-          init: {
-            methodName: 'initialize',
-            args: [poolInfo.setting.ampFactor, poolInfo.setting.haircut], // [A, haircut => 40bps]
-          },
-        },
-      },
+    const { deployResult, contract: pool } = await deployBasePool(contractNamePrefix, poolName, poolInfo, {
+      CoreV3: coreV3Deployment.address,
     })
 
     // Get freshly deployed Pool pool
-    const pool = await ethers.getContractAt(contractNamePrefix, deployResult.address)
-    const implAddr = await upgrades.erc1967.getImplementationAddress(deployResult.address)
-    deployments.log('Contract address:', deployResult.address)
-    deployments.log('Implementation address:', implAddr)
     if (deployResult.newlyDeployed) {
-      await setUpPool(deployerSigner, pool, multisig, poolInfo.setting)
       await configureCrossChainPool(deployerSigner, pool, poolInfo.setting)
-
-      logVerifyCommand(deployResult)
-    } else {
-      deployments.log(`${contractNamePrefix} Contract already deployed.`)
     }
   }
-}
-
-async function setUpPool(
-  deployerSigner: SignerWithAddress,
-  pool: Contract,
-  multisig: string,
-  config: ICrossChainPoolConfig
-) {
-  const masterWombatV3Deployment = await deployments.getOrNull('MasterWombatV3')
-  if (masterWombatV3Deployment?.address) {
-    deployments.log('Setting master wombat: ', masterWombatV3Deployment.address)
-    await confirmTxn(pool.connect(deployerSigner).setMasterWombat(masterWombatV3Deployment.address))
-  }
-  await confirmTxn(pool.connect(deployerSigner).setCovRatioFeeParam(config.startCovRatio, config.endCovRatio))
-  // Check setup config values
-  const ampFactor = await pool.ampFactor()
-  const hairCutRate = await pool.haircutRate()
-  deployments.log(`Amplification factor is : ${ampFactor}`)
-  deployments.log(`Haircut rate is : ${hairCutRate}`)
-
-  // transfer pool contract dev to Gnosis Safe
-  deployments.log(`Transferring dev of ${pool.address} to ${multisig}...`)
-  // The dev of the pool contract can pause and unpause pools & assets!
-  await confirmTxn(pool.connect(deployerSigner).setDev(multisig))
-  deployments.log(`Transferred dev of ${pool.address} to:`, multisig)
-
-  /// Admin scripts
-  deployments.log(
-    `setFee to ${config.lpDividendRatio} for lpDividendRatio and ${config.retentionRatio} for retentionRatio...`
-  )
-  await confirmTxn(pool.connect(deployerSigner).setFee(config.lpDividendRatio, config.retentionRatio))
-
-  deployments.log(`setFeeTo to ${multisig}.`)
-  await confirmTxn(pool.connect(deployerSigner).setFeeTo(multisig))
-
-  deployments.log(`setMintFeeThreshold to ${config.mintFeeThreshold} ...`)
-  await confirmTxn(pool.connect(deployerSigner).setMintFeeThreshold(config.mintFeeThreshold))
 }
 
 async function configureCrossChainPool(
