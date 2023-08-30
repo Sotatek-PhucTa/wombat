75c25893d9125056d58123e9c3981838701ce273
jack <97011001+jack-wombat@users.noreply.github.com>
2023-08-30 06:48:10 -0700

    Multisig to add crosschain pool LPs
    ETH does not have voter, so only add it to master wombat
    
    Verification:
    > for x in bsc arb eth; FORK_NETWORK=$x"_mainnet" hh run scripts/multisig/AddAsset_CrossChain_Pool.ts; end
    
    Generate proposal:
    > for x in bsc arb eth; hh run scripts/multisig/AddAsset_CrossChain_Pool.ts --network $x"_mainnet"; end

---
+++ scripts/multisig/AddAsset_CrossChain_Pool.ts
@@ -0,0 +1,25 @@
+import { runScript } from '.'
+import { Network } from '../../types'
+import { getCurrentNetwork } from '../../types/network'
+import { concatAll } from '../../utils'
+import * as multisig from '../../utils/multisig'
+
+runScript('AddAssets_CrossChain_Pool', async () => {
+  const network = getCurrentNetwork()
+
+  if ([Network.BSC_MAINNET, Network.ARBITRUM_MAINNET].includes(network)) {
+    return concatAll(
+      ...['Asset_Stablecoin_Pool_USDT', 'Asset_Stablecoin_Pool_USDC'].map((assetDeployment) =>
+        multisig.utils.addAssetToMasterWombatAndVoter(assetDeployment, true)
+      )
+    )
+  } else if ([Network.ETHEREUM_MAINNET].includes(network)) {
+    return concatAll(
+      ...['Asset_Stablecoin_Pool_USDT', 'Asset_Stablecoin_Pool_USDC'].map((assetDeployment) =>
+        multisig.utils.addAssetToMasterWombat(assetDeployment)
+      )
+    )
+  } else {
+    throw new Error(`Unsupported network: ${network}`)
+  }
+})
