016c24c868e53aaa367cbb9310695ad5c0f49cd0
jack <97011001+jack-wombat@users.noreply.github.com>
2023-09-11 13:56:37 +0800

    Update zBNB zUSD pool script

--- scripts/multisig/AddAsset_SetBribeDeployer_zBNB_zUSD.ts
+++ scripts/multisig/AddAsset_SetBribeDeployer_zBNB_zUSD.ts
@@ -6,9 +6,9 @@ import * as multisig from '../../utils/multisig'
 
 const ASSETS = ['Asset_zBNB_Pool_zBNB', 'Asset_zBNB_Pool_WBNB', 'Asset_zUSD_Pool_zUSD', 'Asset_zUSD_Pool_USDC']
 
-runScript('AddAssets_zBNB_zUSD_Pool', async () => {
+runScript('AddAsset_SetBribeDeployer_zBNB_zUSD', async () => {
   return concatAll(
-    ...ASSETS.map((assetDeployment) => multisig.utils.addAssetToMasterWombatAndVoter(assetDeployment, false)),
+    ...ASSETS.map((assetDeployment) => multisig.utils.addAssetToMasterWombatAndVoter(assetDeployment, true)),
     ...ASSETS.map((assetDeployment) =>
       multisig.utils.setBribeOperatorForAsset(assetDeployment, ExternalContract.HorizonBribeOperator)
     ),
