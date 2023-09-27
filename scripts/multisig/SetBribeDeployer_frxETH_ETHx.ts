207e75a54ed09acec86a92e6066d15e4b4e7a28e
tj-wombat <129966081+tj-wombat@users.noreply.github.com>
2023-09-27 15:55:57 +0800

    Fix/use the correct bribe operator address for ETHx and frxETH pool assets (#919)
    * update script
    
    * update proposal

--- scripts/multisig/SetBribeDeployer_frxETH_ETHx.ts
+++ scripts/multisig/SetBribeDeployer_frxETH_ETHx.ts
@@ -1,6 +1,5 @@
 import { runScript } from '.'
 import { ExternalContract } from '../../config/contract'
-import { Token } from '../../config/token'
 import { concatAll } from '../../utils'
 import * as multisig from '../../utils/multisig'
 
@@ -10,12 +9,10 @@ const ETHx_ASSETS = ['Asset_ETHx_Pool_ETHx', 'Asset_ETHx_Pool_WETH']
 runScript('SetBribeDeployer_frxETH_ETHx', async () => {
   return concatAll(
     ...frxETH_ASSETS.map((assetDeployment) =>
-      multisig.utils.setBribeOperatorForAsset(assetDeployment, ExternalContract.StaderBribeOperator)
-    ),
-    ...ETHx_ASSETS.map((assetDeployment) =>
       multisig.utils.setBribeOperatorForAsset(assetDeployment, ExternalContract.FraxBribeOperator)
     ),
-    multisig.utils.whitelistRewardToken(Token.SD),
-    multisig.utils.whitelistRewardToken(Token.FXS)
+    ...ETHx_ASSETS.map((assetDeployment) =>
+      multisig.utils.setBribeOperatorForAsset(assetDeployment, ExternalContract.StaderBribeOperator)
+    )
   )
 })
